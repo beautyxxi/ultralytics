92e973d1f60f789e7e0ce50a5dee07a18719f477
zxx <1131633240@qq.com>
2024-10-15 16:24:07 +0800

    have some problem

--- study/main.py
+++ study/main.py
@@ -1,26 +1,30 @@
 from ultralytics import YOLO
 
-# Load a model
-model = YOLO("yolo11n.pt")
+model = YOLO("zxx/v1.yaml")
+
+# # Load a model
+# model = YOLO("yolo11n.pt")    
 
 # Train the model
 train_results = model.train(
-    data="coco8.yaml",  # path to dataset YAML
+    data="custom.yaml",  # path to dataset YAML
     epochs=10,  # number of training epochs
     imgsz=640,  # training image size
     device="cpu",  # device to run on, i.e. device=0 or device=0,1,2,3 or device=cpu
+    batch=1,
 )
 
-# # Evaluate model performance on the validation set
-# metrics = model.val()
+# Evaluate model performance on the validation set
+metrics = model.val()
 
 # Perform object detection on an image
-results = model(
+results = model.predict(
     [
-        "D:\\work\\ultralytics\\ultralytics\\assets\\bus.jpg", 
-        "D:\\work\\ultralytics\\ultralytics\\assets\\zidane.jpg"
+        "D:\\work\\datasets\\custom\\images\\train\\001.jpg",
+        "D:\\work\\datasets\\custom\\images\\train\\002.jpg",
     ]
 )
+print(results)
 results[0].show()
 results[1].show()
 
