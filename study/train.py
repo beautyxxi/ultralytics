319eb4174d1e0c07685deab2d91f85bdb5d47080
zxx <1131633240@qq.com>
2025-02-13 10:57:31 +0800

    upload

--- study/train.py
+++ study/train.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO, RTDETR
 
-# model = YOLO("zxx/yolo11s-AIFI.yaml")  
+# model = YOLO("zxx/yolo11s.yaml")  
 model = RTDETR("zxx/yolo11s-AIFI.yaml")
 
 # Train the model
@@ -9,7 +9,7 @@ train_results = model.train(
     epochs=150,  # number of training epochs
     imgsz=640,  # training image size
     device=0,  # device to run on, i.e. device=0 or device=0,1,2,3 or device=cpu
-    batch=16,
+    batch=8,
 )
 
 # Evaluate model performance on the validation set
