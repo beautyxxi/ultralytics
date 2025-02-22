4f115d78d280c1be83bb70f58f6ee7c81c3e2f3c
yin <421626388@qq.com>
2025-02-22 21:36:46 +0800

    move 'rtdetr-resnet50.yaml' to 'zxx/rtdetr-r50.yaml'

--- study/test.py
+++ study/test.py
@@ -1,39 +1,13 @@
-import warnings
-import os
-from pathlib import Path
-from ultralytics import RTDETR
-import torch
+from ultralytics import YOLO, RTDETR
 
-# warnings.filterwarnings('ignore')  #忽略 Python 运行时的警告（warnings）
+# model = RTDETR("rt-detr/rtdetr-resnet50.yaml")
+model = RTDETR("zxx/rtdetr-r18.yaml").load("rtdetr-r18-f32-28epoch.pt")
 
-
-# def check_path(path):
-#     if not os.path.exists(path):
-#         raise FileNotFoundError(f"Path does not exist: {path}")
-
-
-if __name__ == '__main__':
-    torch.cuda.empty_cache()
-    # # 获取当前脚本所在的目录
-    # current_dir = Path(__file__).parent
-    # 构建相对路径
-    # yaml_path = '/mnt/RTdetr/RTDETR-main/dataset/dataset_visdrone/data.yaml'
-    # check_path(yaml_path)
-    model = RTDETR("zxx/uav-rtdetr.yaml")
-    model.train(
-                # data=str(yaml_path),
-                data="VisDrone.yaml",   # 官方路径
-                # cache=False,
-                imgsz=640,
-                epochs=150,
-                batch=8,
-                workers=8,
-                device='0',
-                # resume='', # last.pt path
-                # project='runs/train',
-                # name='exp',
-                # patience = 40,
-                )
-    
 # Evaluate model performance on the validation set
-metrics = model.val()
+metrics = model.val(
+    data="VisDrone.yaml",
+    imgsz=640,
+    batch=16,
+    device=0,
+    split="val",
+)
--- study/train.py
+++ study/train.py
@@ -1,9 +1,7 @@
 from ultralytics import YOLO, RTDETR
 
-# model = YOLO("zxx/yolo11s.yaml")
-# model = RTDETR("zxx/yolo11s-AIFI.yaml")
-model = RTDETR("rt-detr/rtdetr-resnet50.yaml").load("rtdetr-l.pt")
-# model = RTDETR("zxx/rtdetr-r18.yaml")
+# model = RTDETR("rt-detr/rtdetr-resnet50.yaml")
+model = RTDETR("zxx/rtdetr-r18.yaml")
 
 # Train the model
 train_results = model.train(
@@ -11,23 +9,14 @@ train_results = model.train(
     epochs=150,  # number of training epochs
     imgsz=640,  # training image size
     device=0,  # device to run on, i.e. device=0 or device=0,1,2,3 or device=cpu
-    batch=1,
-    optimizer="AdamW",
+    batch=4, # batch_size
+    amp=False, # 自动混合精度，False为使用FP32
+    lr0=0.0001, # 初始学习率
+    lrf=1.0, # 最终学习率为 lr0 * lrf
+    warmup_epochs=2000, # 预热轮数
+    # cache=False,
+    optimizer="AdamW", # 优化器
 )
 
 # Evaluate model performance on the validation set
 metrics = model.val()
-
-# Perform object detection on an image
-# results = model.predict(
-#     [
-#         "D:\\work\\datasets\\custom\\images\\train\\001.jpg",
-#         "D:\\work\\datasets\\custom\\images\\train\\002.jpg",
-#     ]
-# )
-# print(results)
-# results[0].show()
-# results[1].show()
-
-# # Export the model to ONNX format
-# path = model.export(format="onnx")  # return path to exported model
