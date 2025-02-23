034aff085bf2bae91430c63d391aea981829958e
yin <421626388@qq.com>
2025-02-23 15:01:58 +0800

    fix

--- study/train.py
+++ study/train.py
@@ -12,17 +12,18 @@ model = RTDETR("zxx/rtdetr-r18.yaml")
 # Train the model
 train_results = model.train(
     data="VisDrone.yaml",  # path to dataset YAML
-    epochs=100,  # number of training epochs
+    epochs=200,  # number of training epochs
     imgsz=640,  # training image size
     device=0,  # device to run on, i.e. device=0 or device=0,1,2,3 or device=cpu
-    batch=4, # batch_size
-    amp=False, # 自动混合精度，False为使用FP32
+    batch=8, # batch_size
+    amp=True, # 自动混合精度，False为使用FP32
     lr0=0.0001, # 初始学习率
     lrf=1.0, # 最终学习率为 lr0 * lrf
+    patience=40, # 早停
     cache=False, # 标签是否缓存
     optimizer="AdamW", # 优化器
     project="zxx", # 设置project名称，在wandb下面显示
-    name="rtdetr-r18", # 将本次实验结果记录在project/name下
+    name="rtdetr-r18-amp", # 将本次实验结果记录在project/name下
 )
 
 # Evaluate model performance on the validation set
